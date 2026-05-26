8ca44783d6281c5a5c69b65700a4d617ffb8d98e
Armemius <rusgy@yandex.ru>
2026-05-26 16:26:50 +0300

    feat: provide interface for the tape

--- include/tape/tape.hpp
+++ include/tape/tape.hpp
@@ -1,3 +1,100 @@
 #pragma once
 
-namespace tp {}
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+
+namespace tp {
+
+using namespace std::literals::chrono_literals;
+
+/**
+ * @brief Interface representing the tape
+ */
+class Tape {
+   public:
+    struct LatencyConfig {
+        bool enable_sleep_delays{false};
+        std::chrono::nanoseconds read_delay{1us};
+        std::chrono::nanoseconds write_delay{2us};
+        std::chrono::nanoseconds move_delay{100us};
+        std::chrono::nanoseconds rewind_delay{1ms};
+    };
+
+    enum class TapeOperation { READ, WRITE, MOVE, REWIND };
+
+    struct TapeStats {
+        size_t reads{};
+        size_t writes{};
+        size_t moves{};
+        size_t rewinds{};
+
+        std::chrono::nanoseconds simulated_time{};
+    };
+
+    Tape() = default;
+
+    explicit Tape(LatencyConfig latency_config);
+
+    virtual ~Tape() = default;
+
+    [[nodiscard]] const TapeStats &stats() const noexcept;
+
+    void reset_stats() noexcept;
+
+    /**
+     * @brief Reads the current element the cursor is pointing to
+     */
+    [[nodiscard]] int32_t read();
+
+    /**
+     * @brief Writes the value of element the cursor is pointing to
+     */
+    void write(int32_t value);
+
+    /**
+     * @brief Moves the cursor to the next element
+     */
+    void next();
+
+    /**
+     * @brief Moves the cursor to the prev element
+     */
+    void prev();
+
+    /**
+     * @brief Moves cursor to the start of the tape
+     */
+    void rewind();
+
+    /**
+     * @brief Returns the index of the cell, the cursor is currently on
+     */
+    [[nodiscard]] virtual size_t position() const = 0;
+
+    /**
+     * @brief Returns the number of cells on the tape
+     */
+    [[nodiscard]] virtual size_t size() const = 0;
+
+   protected:
+    // Implementation hooks. Public methods apply latency, then delegate here
+
+    [[nodiscard]] virtual int32_t do_read() = 0;
+    virtual void do_write(int32_t value) = 0;
+    virtual void do_next() = 0;
+    virtual void do_prev() = 0;
+    virtual void do_rewind() = 0;
+
+   private:
+    void delay(std::chrono::nanoseconds duration) const;
+    void account_read();
+    void account_write();
+    void account_move();
+    void account_rewind();
+
+    TapeStats stats_;
+    LatencyConfig latency_config_;
+};
+
+}  // namespace tp
--- src/tape/tape.cpp
+++ src/tape/tape.cpp
@@ -1,1 +1,64 @@
 #include "tape/tape.hpp"
+
+namespace tp {
+
+Tape::Tape(Tape::LatencyConfig latency_config) : latency_config_(latency_config) {}
+
+const Tape::TapeStats &Tape::stats() const noexcept {
+    return stats_;
+}
+
+void Tape::reset_stats() noexcept {
+    stats_ = {};
+}
+
+int32_t Tape::read() {
+    account_read();
+    return do_read();
+}
+
+void Tape::write(int32_t value) {
+    account_write();
+    do_write(value);
+}
+
+void Tape::next() {
+    account_move();
+    do_next();
+}
+
+void Tape::prev() {
+    account_move();
+    do_prev();
+}
+
+void Tape::rewind() {
+    account_rewind();
+    do_rewind();
+}
+
+void Tape::account_read() {
+    ++stats_.reads;
+    stats_.simulated_time += latency_config_.read_delay;
+    delay(latency_config_.read_delay);
+}
+
+void Tape::account_write() {
+    ++stats_.writes;
+    stats_.simulated_time += latency_config_.write_delay;
+    delay(latency_config_.write_delay);
+}
+
+void Tape::account_move() {
+    ++stats_.moves;
+    stats_.simulated_time += latency_config_.move_delay;
+    delay(latency_config_.move_delay);
+}
+
+void Tape::account_rewind() {
+    ++stats_.rewinds;
+    stats_.simulated_time += latency_config_.rewind_delay;
+    delay(latency_config_.rewind_delay);
+}
+
+}  // namespace tp
