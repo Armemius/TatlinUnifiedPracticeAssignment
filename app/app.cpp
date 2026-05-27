a5085164edc44b72f0b393ba7c55c414c01c85c2
Armemius <rusgy@yandex.ru>
2026-05-27 16:42:18 +0300

    feat: add basic executable
    Add executable with hardcoded preset

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,39 @@
+#include <cstdlib>
+#include <exception>
+#include <filesystem>
 #include <iostream>
+#include <memory>
+#include <optional>
+#include "tape/external_merge_tape_sorter.hpp"
+#include "tape/file_tape.hpp"
+#include "utils/tmp_directory.hpp"
 
-int main() {
-    std::cout << "Nothing here yet :)\n";
+int main(int argc, char **argv) {
+    if (argc != 3 && argc != 4) {
+        std::cerr << "Usage: task <input> <output> [config]\n";
+        return EXIT_FAILURE;
+    }
+    std::filesystem::path input_tape_path = argv[1];
+    std::filesystem::path output_tape_path = argv[2];
+    std::optional<std::filesystem::path> config_path = std::nullopt;
+    if (argc == 4) {
+        config_path = argv[3];
+    } else if (auto path = std::filesystem::current_path() / "config.toml"; std::filesystem::exists(path)) {
+        config_path = std::move(path);
+    }
+
+    try {
+        auto temp_dir = tp::utils::TmpDirectory();
+        auto input_tape = std::make_shared<tp::FileTape>(input_tape_path);
+        auto output_tape = std::make_shared<tp::FileTape>(output_tape_path, input_tape->size());
+        auto tape_factory = std::make_shared<tp::FileTapeFactory>(temp_dir.path());
+
+        auto sorter = tp::ExternalMergeTapeSorter(tape_factory, 1024 * 1024 * 256);
+        sorter.sort(input_tape, output_tape);
+    } catch (const std::exception &ex) {
+        std::cerr << "Error while processing: " << ex.what() << "\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
